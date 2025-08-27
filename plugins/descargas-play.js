59158b797c794d1696ac252b5cd4b5d51ea15257
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-26 21:40:55 -0700

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -22,8 +22,8 @@ const handler = async (m, { conn, command, text }) => {
     caption,
     footer: "📥 Elige una opción para descargar",
     buttons: [
-      { buttonId: `${usedPrefix} ytmp3 ${video.url}`, buttonText: { displayText: "🎧 Descargar MP3" }, type: 1 },
-      { buttonId: `${usedPrefix} ytmp4 ${video.url}`, buttonText: { displayText: "🎬 Descargar MP4" }, type: 1 }
+      { buttonId: `${usedPrefix} ytmp3 ${videoInfo.url}`, buttonText: { displayText: "🎧 Descargar MP3" }, type: 1 },
+      { buttonId: `${usedPrefix} ytmp4 ${videoInfo.url.url}`, buttonText: { displayText: "🎬 Descargar MP4" }, type: 1 }
     ],
     headerType: 4
   }, { quoted: m })
