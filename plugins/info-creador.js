b70775831ab34cadc99a37e57a7e0492f7ba3f68
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 15:09:25 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -8,9 +8,6 @@ let handler = async (m, { conn }) => {
       displayName: "Chinchu dzn",
       vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Chinchu\nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;; Argentina 🇦🇷;;;;\nitem4.X-ABLabel:País\nEND:VCARD` },
     {
-      displayName: "Jota",
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Colombia 🇨🇴;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
-    }
   ];
 
   await conn.sendMessage(m.chat, {
