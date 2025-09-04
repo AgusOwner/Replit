ce3d33bb3161b6e99d9dc07bb5d935098755f8a4
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-04 13:39:27 -0500

    Crear fun-chincu.js

---
+++ plugins/fun-chincu.js
@@ -0,0 +1,12 @@
+const handler = async (m, { conn }) => {
+
+
+  conn.sendMessage(m.chat, {
+text: `Mi dueña es Chinchu dzn y el dueño de ella es Jota🐼🥵`,
+}, { quoted: fkontak });
+};
+
+handler.tags = ['fun'];
+handler.help = ['diversion'];
+handler.command = ['chinchu'];
+export default handler;
