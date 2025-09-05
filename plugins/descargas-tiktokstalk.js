bf3bbe9f9cd47e3e824327bb08344dacaee222ef
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-05 00:18:43 -0500

    Actualizar descargas-tiktokstalk.js

--- plugins/descargas-tiktokstalk.js
+++ plugins/descargas-tiktokstalk.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) {
-        throw m.reply(`*${xdownload} Por favor, ingresa un link de TikTok.*\n> *\`Ejemplo:\`* ${usedPrefix + command} https://vm.tiktok.com/ZMkcuXwJv/`);
+        throw m.reply(`*no sé encontró este video Por favor, ingresa un link de TikTok.*\n> *\`Ejemplo:\`* ${usedPrefix + command} https://vm.tiktok.com/ZMkcuXwJv/`);
     }
 
     try {
