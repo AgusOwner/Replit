3aa699a596b246cc5ac085f68535ffebba5854b8
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-04 20:15:58 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,26 +1,38 @@
-import fetch from 'node-fetch'
+const handler = async (m, {conn, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.info_creador
 
-let handler = async (m, { conn }) => {
-  await m.react('🫟');
-
-  let list = [
-    {
-      displayName: "Chinchu dzn",
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Chinchu\nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;; Argentina 🇦🇷;;;;\nitem4.X-ABLabel:País\nEND:VCARD` },
-    {
-
-  ];
-
-  await conn.sendMessage(m.chat, {
-    contacts: {
-      displayName: `${list.length} Contactos`,
-      contacts: list
-    }
-  }, { quoted: estilo });
+  const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const text = `${tradutor.texto1[0]}`.trim();
+  const buttonMessage= {
+    'document': {url: `https://www.instagram.com/ineffable.mvrco/`},
+    'mimetype': `application/${document}`,
+    'fileName': `${tradutor.texto2[0]}`,
+    'fileLength': 99999999999999,
+    'pageCount': 200,
+    'contextInfo': {
+      'forwardingScore': 200,
+      'isForwarded': true,
+      'externalAdReply': {
+        'mediaUrl': 'https://www.instagram.com/ineffable.mvrco/',
+        'mediaType': 2,
+        'previewType': 'pdf',
+        'title': tradutor.texto2[1],
+        'body': wm,
+        'thumbnail': imagen1,
+        'sourceUrl': 'https://chat.whatsapp.com/FFB0fgdK3IP4ZEmZ8jF1CC'}},
+    'caption': text,
+    'footer': wm,
+    // 'buttons':[
+    // {buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1},
+    // {buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
+    'headerType': 6};
+  conn.sendMessage(m.chat, buttonMessage, {quoted: m});
 };
-
-handler.help = ['creador', 'dueño'];
+handler.help = ['owner', 'creator'];
 handler.tags = ['info'];
-handler.command = /^(owner|creador|creator|dueño|desarrollador)$/i;
-
+handler.command = /^(owner|creator|creador|propietario)$/i;
 export default handler;
