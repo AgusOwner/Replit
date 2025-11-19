5ef26fe53657a4ef9b7f85c6961df63aa4df9dc3
Jotaahrz <rodriguezjota90@gmail.com>
2025-11-19 00:40:20 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,64 +1,80 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
+// contacto citado como fkontak
+const fkontak = {
+  key: {
+    participants: "0@s.whatsapp.net",
+    remoteJid: "status@broadcast",
+    fromMe: false,
+    id: "Bienvenida"
+  },
+  message: {
+    contactMessage: {
+      displayName: "JotaBot🐼",
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:JotaBot🐼\nORG:JotaBot\nTEL;type=CELL;type=VOICE;waid=00000000000:+00 00000000\nEND:VCARD`
+    }
+  }
+}
+
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
 
-  let insta = 'https://instagram.com/ineffable.mvrco'
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
-  }
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/Y73rRC0X')
-  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = `¡Bienvenid@! ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo.`
-  let txt1 = `¡Adiós! ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo`
-  let txt2 = `Se salió ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo.`
-  let sunflare = `ゲ◜៹ New Member ៹◞ゲ`
-  let sunflare1 = `ゲ◜៹ Kicked Member ៹◞ゲ`
-  let sunflare2 = `ゲ◜៹ Bye Member ៹◞ゲ`
+  let defaultImage = 'https://cdn.russellxz.click/5d5384cd.jpeg'
+  let dev = 'Adhara Bot by Jotaa.hrz'
 
-  if (chat.welcome && m.messageStubType == 27) {
-    const groupName = groupMetadata.subject
-    const groupDesc = groupMetadata.desc || 'sin descripción'
+  if (!chat.customWelcome) chat.customWelcome = null
+  if (!chat.customBye) chat.customBye = null
+  if (chat.welcome === undefined) chat.welcome = true
 
-    let bienvenida = chat.sWelcome
-      ? chat.sWelcome
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-      : `*¡Bienvenido(a)!*\n෫ࣲׄ֟፝͡${taguser} ☕꒱\n\nᦷᩘᦷ   ݂ 𝖣𝗂𝗌𝖿𝗋𝗎𝗍𝖺 𝖽𝖾 𝗍𝗎 𝖾𝗌𝗍𝖺𝖽𝗂𝖺.\n✎ 𝖴𝗌𝖺 *#help* 𝗉𝖺𝗋𝖺 𝗏𝖾𝗋 𝗅𝖺 𝗅𝗂𝗌𝗍𝖺 𝖽𝖾 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌.\n\n> ${dev}`
-    await conn.sendLuffy(m.chat, txt, sunflare, bienvenida, img, img, insta, fkontak)
+  // Si el sistema de bienvenida está desactivado, no hacer nada
+  if (!chat.welcome) return true
+
+  let img
+  try {
+    let pp = await conn.profilePictureUrl(who, 'image')
+    img = await (await fetch(pp)).buffer()
+  } catch {
+    img = await (await fetch(defaultImage)).buffer()
   }
 
-  if (chat.welcome && m.messageStubType == 28) {
-    const groupName = groupMetadata.subject
-    const groupDesc = groupMetadata.desc || 'sin descripción'
+  if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let bienvenida = chat.customWelcome
+      ? chat.customWelcome.replace(/@user/g, taguser).replace(/@group/g, groupMetadata.subject)
+      : `૮₍⸝⸝> ̫ <⸝⸝₎ა ♡ ¡Holaaa @${taguser} !
+Bienvenid@ a *${groupMetadata.subject}* 🌸
 
-    let ban = chat.sKick
-      ? chat.sKick
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-      : `*¡Expulsado!*\n෫ࣲׄ֟፝͡${taguser} 👊🏻꒱\n\nᏊ⁠ 𝖴𝗇 𝗇𝖾𝗀𝗋𝗈 𝗆𝖾𝗇𝗈𝗌 𝖾𝗇 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈, 𝗉𝗈𝗋 𝗇𝗈 𝗈𝖻𝖾𝖽𝖾𝖼𝖾𝗋 𝗅𝖺𝗌 𝗋𝖾𝗀𝗅𝖺𝗌.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗅𝖺 𝖾𝗅𝗂𝗆𝗂𝗇𝖺𝖼𝗂𝗈𝗇 𝗅𝖾 𝗁𝖺𝗀𝖺 𝗋𝖾𝖿𝗅𝖾𝗑𝗂𝗈𝗇𝖺𝗋 𝗑𝖣\n\n> ${dev}`
-    await conn.sendLuffy(m.chat, txt1, sunflare1, ban, img, img, insta, fkontak)
+🌷✨ Qué ternura tenerte por aquí.
+Espero que tu estancia sea súper linda y te sientas como en casa. 🍡💕
+
+> ${dev}`
+    await conn.sendMessage(
+      m.chat,
+      { image: img, caption: bienvenida, mentions: [who] },
+      { quoted: fkontak } // aquí va el contacto citado
+    )
+  } else if (
+    m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE ||
+    m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE
+  ) {
+    let bye = chat.customBye
+      ? chat.customBye.replace(/@user/g, taguser).replace(/@group/g, groupMetadata.subject)
+      : `૮₍ • ᴥ • ₎ა ♡ ¡Hasta pronto ${taguser} !
+Gracias por estar en *${groupMetadata.subject}* 🌸
+
+🌷✨ Fue muy lindo tenerte por aquí.
+Te mando vibras suaves y un abrazo lleno de ternurita. 🍡💕
+
+> ${dev}`
+    await conn.sendMessage(
+      m.chat,
+      { image: img, caption: bye, mentions: [who] },
+      { quoted: fkontak } // aquí también
+    )
   }
 
-  if (chat.welcome && m.messageStubType == 32) {
-    const groupName = groupMetadata.subject
-    const groupDesc = groupMetadata.desc || 'sin descripción'
-
-    let bye = chat.sBye
-      ? chat.sBye
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-      : `*¡Hasta Luego!*\n෫ࣲׄ֟፝͡${taguser} 👋🏻꒱\n\nᏊ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ 𝖦𝗋𝖺𝖼𝗂𝖺𝗌 𝗉𝗈𝗋 𝖾𝗌𝗍𝖺𝗋 𝖺𝗊𝗎𝗂. 𝖳𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖿𝗎𝖾 𝗎𝗇 𝖾𝗌𝗍𝗈𝗋𝖻𝗈.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗌𝖾 𝗅𝗈 𝗏𝗂𝗈𝗅𝖾𝗇 𝗅𝗈𝗌 𝗇𝖾𝗀𝗋𝗈𝗌 𝗉𝗈𝗋 𝖺𝗅𝗍𝖺 𝗉𝗎𝗍𝖺.\n\n> ${dev}`
-    await conn.sendLuffy(m.chat, txt1, sunflare2, bye, img, img, insta, fkontak)
-  }
+  return true
 }
